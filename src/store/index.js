5fb9539099e53e5c6140f3ac9f69a0207b870b88
吴岩 <3122523963@qq.com>
2019-11-28 13:10:22 +0800

    调用接口封装

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
+
 Vue.use(Vuex);
-Vue.use(ElementUI);
 
 export default new Vuex.Store({
   state: {},
